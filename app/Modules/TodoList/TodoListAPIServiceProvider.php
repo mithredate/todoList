89cca0eacebc97963e4a542ea79712103c92808b
Mehrdad Hedayati <mehrdad.hedayati@gmail.com>
2016-10-22 23:53:55 +0330

    Marked CollectionResponse and ItemResponse singleton

--- app/Modules/TodoList/TodoListAPIServiceProvider.php
+++ app/Modules/TodoList/TodoListAPIServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Modules\TodoList;
 
+use App\Modules\TodoList\Contracts\TodoListRepository;
+use App\Modules\TodoList\Http\CollectionResponse;
+use App\Modules\TodoList\Http\ItemResponse;
 use App\Modules\TodoList\Models\TodoList;
 use App\Modules\TodoList\Policies\TodoListPolicy;
+use App\Modules\TodoList\Repositories\EloquentTodoListRepository;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
@@ -37,8 +41,11 @@ class TodoListAPIServiceProvider extends ServiceProvider
             require dirname(__FILE__) . '/routes.php';
         });
 
-        $this->app->bind('App\Modules\TodoList\Contracts\TodoListRepository',
-            'App\Modules\TodoList\Repositories\EloquentTodoListRepository');
+        $this->app->bind(TodoListRepository::class,
+            EloquentTodoListRepository::class);
+
+        $this->app->singleton(CollectionResponse::class);
+        $this->app->singleton(ItemResponse::class);
 
         $factory->macro('collectionJson',function($value, $status) use ($factory){
            return $factory->make(json_encode($value))
